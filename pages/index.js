bbd523edca8630d5295c0bce76a9eafcd95fecc5
fixonz <109214021+fixonz@users.noreply.github.com>
2025-02-12 17:03:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,12 @@ function Home({ title, img, description, nfts, pages, filters }) {
   return (
     <div
       className="flex flex-col items-center justify-center 
-    min-h-screen bg-gradient-to-r from-black to-gray-100 h-full"
+    min-h-screen bg-gradient-to-r from-green-800 to-green-100 h-full"
       ref={ref}
     >
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="./transparentpepe.png" /> {/* Set favicon here */}
+        <link rel="icon" href="/transparentpepe.png" /> {/* Corrected path */}
       </Head>
 
       <NextSeo
@@ -46,7 +46,8 @@ function Home({ title, img, description, nfts, pages, filters }) {
       <Navbar
         title={
           <>
-            <img src="./transparentpepe.png" // Path to your image
+            <img
+              src="/transparentpepe.png" // Corrected path
               alt="Logo"
               className="h-8 w-auto mr-2 inline" // Adjust size as needed
             />
