1fb70a52a2fce7f105158795dbe43b792d897884
fixonz <109214021+fixonz@users.noreply.github.com>
2025-02-12 19:19:54 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <div className="flex border-t-1 border w-full p-4 mt-4 items-center justify-center">
       <a
-        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-white hover:text-green-500 transition-colors duration-200"
         href="https://x.com/Abstract_Pepes"
         target="_blank"
         rel="noopener noreferrer"
@@ -17,7 +17,7 @@ export const Footer = () => {
         Twitter
       </a>
       <a
-        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-white hover:text-green-500 transition-colors duration-200"
         href="https://discord.gg/ZutUw5Va"
         target="_blank"
         rel="noopener noreferrer"
@@ -28,7 +28,7 @@ export const Footer = () => {
         Discord
       </a>
       <a
-        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-white hover:text-green-500 transition-colors duration-200"
         href="https://abscan.org/address/0xc97cbba151ed084ed49a0c53b93a8f2e805c5489"
         target="_blank"
         rel="noopener noreferrer"
@@ -36,7 +36,7 @@ export const Footer = () => {
         <span className="text-lg mr-1 text-green-400 hover:text-green-500">
           <FaEthereum />
         </span>
-        AbSCAN
+        AbstractScan
       </a>
     </div>
   );
