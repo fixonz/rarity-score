fdb56adbb9b63b15cf8b0e2a901c7ce84f018935
fixonz <109214021+fixonz@users.noreply.github.com>
2025-02-12 19:17:15 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { FiCoffee } from "react-icons/fi";
-import { IoLogoGithub, IoLogoTwitter } from "react-icons/io";
-import { FaDiscord, FaEthereum } from "react-icons/fa"; // Import Discord and Ethereum icons
+import { IoLogoTwitter } from "react-icons/io";
+import { FaDiscord, FaEthereum } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <div className="flex flex-col border-t-1 border w-full p-4 mt-4 items-center text-xs justify-center">
+    <div className="flex border-t-1 border w-full p-4 mt-4 items-center justify-center">
       <a
-        className="flex items-center mb-4 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
         href="https://x.com/Abstract_Pepes"
         target="_blank"
         rel="noopener noreferrer"
@@ -15,10 +14,10 @@ export const Footer = () => {
         <span className="text-lg mr-1 text-green-400 hover:text-green-500">
           <IoLogoTwitter />
         </span>
-        {/* Twitter */}
+        Twitter
       </a>
       <a
-        className="flex items-center mb-4 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
         href="https://discord.gg/ZutUw5Va"
         target="_blank"
         rel="noopener noreferrer"
@@ -26,10 +25,10 @@ export const Footer = () => {
         <span className="text-lg mr-1 text-green-400 hover:text-green-500">
           <FaDiscord />
         </span>
-        {/* Discord */}
+        Discord
       </a>
       <a
-        className="flex items-center mb-4 text-gray-600 hover:text-green-500 transition-colors duration-200"
+        className="flex items-center mx-2 text-gray-600 hover:text-green-500 transition-colors duration-200"
         href="https://abscan.org/address/0xc97cbba151ed084ed49a0c53b93a8f2e805c5489"
         target="_blank"
         rel="noopener noreferrer"
@@ -37,7 +36,7 @@ export const Footer = () => {
         <span className="text-lg mr-1 text-green-400 hover:text-green-500">
           <FaEthereum />
         </span>
-        {/* AbSCAN */}
+        AbSCAN
       </a>
     </div>
   );
