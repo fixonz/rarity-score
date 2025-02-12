eb281a79a9c8c5e50603cf4ef88ece9b594c1837
fixonz <109214021+fixonz@users.noreply.github.com>
2025-02-12 16:32:04 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
   COLLECTION_NAME: "Abstract PEPES",
   COLLECTION_TITLE: "Abstract PEPES",
   COLLECTION_DESCRIPTION: '2,222 collectible PEPE characters representing the NFT culture on Abstract chain.',
-  COLLECTION_IMG_LINK: "https://pbs.twimg.com/profile_banners/1870283865727602689/1734893502/1500x500.jpg",
+  COLLECTION_IMG_LINK: "https://cdn.discordapp.com/attachments/1284769507130478605/1339242341315383366/mage33.png?ex=67ae0220&is=67acb0a0&hm=8d876b58343b1b39f73750a82275fda5a223525f8215c77decbbec4dd03155ec&",
   CONTRACT: {
     // if you're not sure, set CONTRACT: {}, will remove opensea link
     CHAIN: "abstract", //possible values of ethereum, matic (polygin), klatyn, solana
