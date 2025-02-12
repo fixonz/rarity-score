efe4bb9f53b41ac47170e45f821ed709799a1fa8
fixonz <109214021+fixonz@users.noreply.github.com>
2025-02-12 16:08:23 +0200

    Update AttrFilter.js

--- components/AttrFilter.js
+++ components/AttrFilter.js
@@ -38,7 +38,7 @@ export const AttrFilter = (props) => {
             <div className="px-2">
               {filters.map((filter) => (
                 <a
-                  className={`bg-white hover:bg-gray-300 rounded-md cursor-pointer text-gray-800 py-2 px-1 ml-2 w-full flex`}
+                  className={`bg-black hover:bg-white-300 rounded-md cursor-pointer text-gray-800 py-2 px-1 ml-2 w-full flex`}
                   onClick={() => handleChange(filter)}
                 >
                   {filter} ({attrCount[filter]})
